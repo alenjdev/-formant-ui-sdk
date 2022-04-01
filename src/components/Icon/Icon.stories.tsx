6f122d50ce94cae28f53375cf13a853563ee6450
alenjdev <alen@formant.io>
2022-04-01 16:04:13 -0500

    icons started

--- src/components/Icon/Icon.stories.tsx
+++ src/components/Icon/Icon.stories.tsx
@@ -12,6 +12,18 @@ const Template: ComponentStory<typeof Icon> = (args) => <Icon {...args} />;
 
 export const Menu = Template.bind({});
 Menu.args = {
-  size: 20,
+  size: "medium",
   name: "menu",
+};
+
+export const Device = Template.bind({});
+Device.args = {
+  size: "medium",
+  name: "device",
+};
+
+export const Access = Template.bind({});
+Access.args = {
+  size: "large",
+  name: "access",
 };
