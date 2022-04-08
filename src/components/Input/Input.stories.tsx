5d55a2245efabd37047bc59c723b76550e5f2043
alenjdev <alen@formant.io>
2022-04-08 18:59:35 -0500

    input fixed and added style types

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -10,8 +10,40 @@ export default {
 
 const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
 
+export const Large = Template.bind({});
+Large.args = {
+  type: "text",
+  label: "Name",
+};
+
+export const LargeError = Template.bind({});
+LargeError.args = {
+  name: "test",
+  type: "text",
+  label: "Name",
+  error: true,
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  type: "password",
+  label: "Name",
+  inputType: "small",
+};
+
+export const SmallError = Template.bind({});
+SmallError.args = {
+  type: "number",
+  label: "Name",
+  inputType: "small",
+  error: true,
+};
+
 export const Simple = Template.bind({});
 Simple.args = {
   type: "text",
   label: "Name",
+  inputType: "simple",
+  helperText: "helper text",
+  placeHolder: "Email",
 };
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -2,23 +2,67 @@ import * as React from "react";
 import styles from "./Input.module.scss";
 
 export interface IInputProps {
-  type: string;
-  onChange: () => void;
+  type?: string;
   label: string;
+  inputType?: "large" | "small" | "simple";
+  error?: boolean;
+  name?: string;
+  helperText?: string;
+  placeHolder?: string;
+  className: string;
+  onChange:
+    | React.Dispatch<React.SetStateAction<string>>
+    | ((_: string) => void);
 }
 
-const Input: React.FC<IInputProps> = ({ type, label, onChange }) => {
+const Input: React.FC<IInputProps> = ({
+  type,
+  label,
+  inputType,
+  error,
+  name,
+  helperText,
+  placeHolder,
+  className,
+  onChange,
+}) => {
   return (
-    <span className={styles.field}>
-      <input
-        className={styles["field-input"]}
-        onChange={onChange}
-        type={type}
-      />
-      <label id="la" className={styles["field-label"]}>
-        {label}
-      </label>
-    </span>
+    <React.Fragment>
+      {inputType === "simple" ? (
+        <div className={`${styles.simple} ${className}`}>
+          <input
+            className={`${styles["simple-input"]} ${
+              styles[`simple-input-${error ? "error" : "normal"}`]
+            }`}
+            placeholder={placeHolder}
+          />
+          {helperText !== undefined && <span>{helperText}</span>}
+        </div>
+      ) : (
+        <span
+          className={`${styles.field} ${
+            styles[`field-${error ? "error" : "normal"}`]
+          } ${className} `}
+        >
+          <input
+            id={name}
+            className={`${styles["field-input"]} 
+          ${styles[`field-input-${inputType}`]}
+          `}
+            onChange={(ev) => onChange(ev.target.value)}
+            type={type}
+            placeholder=" "
+          />
+          <label
+            htmlFor={name}
+            className={`
+        ${styles["field-label"]} ${styles[`field-label-${inputType}`]}`}
+          >
+            {label}
+          </label>
+        </span>
+      )}
+    </React.Fragment>
   );
 };
 
